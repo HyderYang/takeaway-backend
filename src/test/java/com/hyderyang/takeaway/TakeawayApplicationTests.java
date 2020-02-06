cac2b275d99d8e4267f65de6d6c5831302f82116
hyder <yanghuan0613@gmail.com>
2020-02-07 01:51:18 +0800

    logback 配置

--- src/test/java/com/hyderyang/takeaway/TakeawayApplicationTests.java
+++ src/test/java/com/hyderyang/takeaway/TakeawayApplicationTests.java
@@ -1,13 +1,26 @@
 package com.hyderyang.takeaway;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+@Data
 class TakeawayApplicationTests {
+
 
 	@Test
 	void contextLoads() {
+		String name = "imooc";
+		String password = "123";
+		log.error("error");
+		log.warn("warning");
+		log.info("name: {}, password: {}", name, password);
 	}
 
 }
